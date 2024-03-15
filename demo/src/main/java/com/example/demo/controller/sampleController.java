d14467c24158629aa9b248d2633077d9a5343b1d
shoki <shoki.routine@gmail.com>
2024-03-15 05:13:41 +0000

    fix first

---
+++ demo/src/main/java/com/example/demo/controller/sampleController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class sampleController {
+
+    public String hello() {
+        return "index";
+    }
+}
