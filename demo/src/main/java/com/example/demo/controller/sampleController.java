ae91b60d0ba1b7a6b6307b850195831c786c7f48
shoki <shoki.routine@gmail.com>
2024-03-15 09:42:54 +0000

    add pages

--- demo/src/main/java/com/example/demo/controller/sampleController.java
+++ demo/src/main/java/com/example/demo/controller/sampleController.java
@@ -7,7 +7,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class sampleController {
 
-    public String hello() {
+    public String login() {
         return "index";
     }
+
+    @RequestMapping("/list")
+    public String list() {
+        return "list";
+    }
+
+    @RequestMapping("/create")
+    public String create() {
+        return "create";
+    }
+
+    @RequestMapping("/improvement")
+    public String improvement() {
+        return "improvement";
+    }
+
+    @RequestMapping("/schedule")
+    public String schedule() {
+        return "schedule";
+    }
+
+    @RequestMapping("/result")
+    public String result() {
+        return "result";
+    }
+
+    @RequestMapping("/management")
+    public String management() {
+        return "management";
+    }
+
 }
